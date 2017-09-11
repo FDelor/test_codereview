396af413417768f2e862bf6ec4c7a868b81248a2
root <fdelor@idexcorp.com>
2017-09-11 06:30:15 -0700

    make factory thread save

--- CodeReviewTest/Faktory.h
+++ CodeReviewTest/Faktory.h
@@ -4,6 +4,7 @@
 #include <QSharedPointer>
 #include <QWeakPointer>
 #include <QHash>
+#include <QMutex>
 
 template<typename Type,typename KeyType>
 class Faktory
@@ -11,27 +12,27 @@ class Faktory
 public:
 
     Faktory(){}
-    ~Factory(){
+
+    virtual ~Faktory(){
         m_instances.clear();
     }
 
     QSharedPointer<Type> getInstance(KeyType key){
         QSharedPointer<Type> inst ;
+        QMutexLocker lock(m_mutex);
+        Q_UNUSED(lock);
         if(m_instances.contains(key)){
             inst = m_instances.value(key).toStrongRef();
         }
         if(inst.isNull()){
             inst.reset(new Type());
-            m_instances.insert(type, inst.toWeakRef());
+            m_instances.insert(inst, inst.toWeakRef());
         }
         return inst;
-
     }
 
-
-
-
     QHash<KeyType, QWeakPointer<Type>> m_instances;
+    QMutex m_mutex;
 };
 
 #endif // FAKTORIY_H
